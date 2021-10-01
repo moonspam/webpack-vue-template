1819244d0875e8d761cc9c3b19b524642bfbbbd0
moonspam <booboo0412@gmail.com>
2021-10-01 10:55:50 +0900

    🛠 package update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
+  extends: 'airbnb-base',
+  plugins: [
+    'import',
   ],
-  "env": {
-    "browser": ["last 2 versions", "safari >= 7"],
-    "jquery": true
+  env: {
+    browser: ['last 2 versions', 'safari >= 7'],
+    jquery: true,
   },
-  "rules": {
-    "no-console": 0,
-    "no-alert": 0,
-    "no-var": 0,
+  rules: {
+    'no-console': 0,
+    'no-alert': 0,
+    'no-var': 0,
   },
-  "globals": {
-    "$": false
-  }
+  globals: {
+    $: false,
+  },
 };
